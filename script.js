df844e6bcbb69cff42d9bf5283d65e39e524e2ed
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-06-27 11:51:17 +1200

    toFixed(2) total balance

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
